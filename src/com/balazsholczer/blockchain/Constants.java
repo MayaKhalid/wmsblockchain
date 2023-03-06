0eeadbb6d668ca3d356eed8ccf99c507a1d968d1
MayaKhalid <119127993+MayaKhalid@users.noreply.github.com>
2023-03-06 10:50:39 +0300

    Add files via upload

---
+++ src/com/balazsholczer/blockchain/Constants.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.balazsholczer.blockchain;
+
+/**
+ *
+ * @author mayak
+ */
+class Constants {
+
+    static String GENESIS_PREV_HASH;
+    static int DIFFICULTY;
+    
+}
